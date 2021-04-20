02cf1825fdc3453c36b559a3233dba8d0bd3551c
sanketmuchhala <61338448+sanketmuchhala@users.noreply.github.com>
2021-04-20 15:27:50 +0530

    Update transfermoney.php

--- transfermoney.php
+++ transfermoney.php
@@ -68,7 +68,7 @@
             </div> 
          </div>
          <footer class="text-center mt-5 py-2" style = "background-color: #0000";>
-        <p style = "color: white";> <strong>&copy 2021. Made by <b> Sanket Muchhala </b><br> Sparks Bank <br></strong></p>
+        <p style = "color: white";> <strong>&copy 2021. Made by <b>Sanket Muchhala</b><br> Sparks Bank <br></strong></p>
          </footer>
          <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script> 
